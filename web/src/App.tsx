ff17b9f97062d3b65de8ebc12d37cb4ddf098bed
TechNik <aniket.97.mane@gmail.com>
2022-11-12 14:29:00 +0530

    Added navbar and routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+
+import Navbar from './components/common/navbar';
+import Projects from './pages/projects/list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <p>Wassup</p>
-        </div>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
