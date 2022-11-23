f8a1da3944ba41a8c8ff07030154df341be989ab
TechNik <aniket.97.mane@gmail.com>
2022-11-23 20:18:08 +0530

    login user service

--- web/src/services/users/user_service.ts
+++ web/src/services/users/user_service.ts
@@ -1,10 +1,18 @@
 import qs from 'qs';
 
-import { IUser, IAuthUser } from '../../interfaces/users';
+import { IUser, IAuthUser, ILoginUser } from '../../interfaces/users';
 import http from '../../utilities/http';
 
 export const registerUser = (formData: IAuthUser) => {
     let data = qs.stringify(formData)
     http
-        .post<IAuthUser>("/authentication/register", data)
+        .post<IAuthUser>("/authentication/register", data);
+}
+
+export const loginUser = async (formData: ILoginUser) => {
+    let data = qs.stringify(formData)
+    const res = await http
+        .post<ILoginUser>("/authentication/login", data);
+        
+    return res;
 }
