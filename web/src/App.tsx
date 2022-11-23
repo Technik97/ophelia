51e2c5c577a40bd6fbe28d96d371302fce57e898
TechNik <aniket.97.mane@gmail.com>
2022-11-23 20:22:02 +0530

    login route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 
 import Navbar from './components/common/navbar';
 import Projects from './pages/projects/list';
+import LoginUser from './pages/users/login';
 import RegisterUser from './pages/users/new';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/projects' element={<Projects />} />
           <Route path='/register' element={<RegisterUser />} />
+          <Route path='/login' element={<LoginUser />} /> 
         </Routes>
       </Router>
     </>
